c381f019b30d220c8378ee79cda0d5c0493a4055
tanhongit <tannp27@gmail.com>
2022-08-01 06:59:09 +0700

    create function to install package list

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -28,66 +28,6 @@ if [ "" = "$PKG_OK" ]; then
     chsh -s /bin/$REQUIRED_PKG
 fi
 
-echo "=========================== terminator ==========================="
-REQUIRED_PKG="terminator"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt-get install -y $REQUIRED_PKG
-fi
-
-echo "=========================== nano ==========================="
-REQUIRED_PKG="nano"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt-get install -y $REQUIRED_PKG
-fi
-
-sudo apt-get install -y vim-gtk3
-
-echo "=========================== wget ==========================="
-REQUIRED_PKG="wget"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt install -y $REQUIRED_PKG
-fi
-
-echo "=========================== curl ==========================="
-REQUIRED_PKG="curl"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt install -y $REQUIRED_PKG
-fi
-
-echo "=========================== nodejs, npm, deno ==========================="
-REQUIRED_PKG="nodejs"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt-get -y install $REQUIRED_PKG
-fi
-sudo npm cache clean -f
-sudo npm install -g n stable
-sudo npm install -g npm
-
-REQUIRED_PKG="npm"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt-get -y install $REQUIRED_PKG
-fi
-
-sudo apt-get install -y deno
-
 echo "=========================== imwheel ==========================="
 REQUIRED_PKG="imwheel"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
@@ -117,53 +57,8 @@ if [ "" = "$PKG_OK" ]; then
     sodo $REQUIRED_PKG plugins:install heroku-accounts
 fi
 
-echo "=========================== gnome-tweaks ==========================="
-REQUIRED_PKG="gnome-tweaks"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt -y install $REQUIRED_PKG
-fi
-
 echo "=========================== set time on dual boot system ==========================="
 timedatectl set-local-rtc 1 --adjust-system-clock
-
-echo "=========================== Snap ==========================="
-REQUIRED_PKG="snapd"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt install -y $REQUIRED_PKG
-fi
-
-echo '####################################################################'
-echo '############################# softwares ############################'
-echo '####################################################################'
-echo ''
-
-echo "=========================== gparted ==========================="
-REQUIRED_PKG="gparted"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt install -y $REQUIRED_PKG
-fi
-
-echo "=========================== playonlinux ==========================="
-REQUIRED_PKG="playonlinux"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt install -y $REQUIRED_PKG
-fi
-
-echo "=========================== chrome ==========================="
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 echo "=========================== psensor ==========================="
 REQUIRED_PKG="psensor"
@@ -171,12 +66,43 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install 
 echo Checking for $REQUIRED_PKG: $PKG_OK
 if [ "" = "$PKG_OK" ]; then
     echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-
     sudo apt-get install -y lm-sensors hddtemp
     sudo dpkg-reconfigure hddtemp
     sudo sensors-detect
     sudo apt-get install -y $REQUIRED_PKG
 fi
+
+echo '####################################################################'
+echo '######################### Run package list #########################'
+echo '####################################################################'
+echo ''
+
+installPackages() {
+    PACKAGE_LIST=("curl" "wget" "vim" "tmux" "nano" "filezilla" "terminator" "nodejs" "npm" "gnome-tweaks" "snapd" "gparted" "playonlinux" "bleachbit" "dconf-editor" "chrome-gnome-shell")
+
+    for packageName in "${PACKAGE_LIST[@]}"; do
+        echo "=========================== $packageName ==========================="
+        REQUIRED_PKG=$packageName
+        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
+        echo Checking for $REQUIRED_PKG: $PKG_OK
+        if [ "" = "$PKG_OK" ]; then
+            echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+            sudo apt-get install -y $REQUIRED_PKG
+        fi
+        echo ""
+    done
+}
+installPackages
+
+sudo npm cache clean -f
+sudo npm install -g n stable
+sudo npm install -g npm
+
+sudo apt-get install -y deno
+
+echo "=========================== chrome ==========================="
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 echo "=========================== ulauncher ==========================="
 REQUIRED_PKG="ulauncher"
@@ -184,40 +110,13 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install 
 echo Checking for $REQUIRED_PKG: $PKG_OK
 if [ "" = "$PKG_OK" ]; then
     echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-
     sudo add-apt-repository ppa:agornostal/$REQUIRED_PKG
     sudo apt -y update
     sudo apt -y install $REQUIRED_PKG
 fi
 
-echo "=========================== Bleachbit ==========================="
-REQUIRED_PKG="bleachbit"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt -y install $REQUIRED_PKG
-fi
-
-echo "=========================== dconf-editor ==========================="
-REQUIRED_PKG="dconf-editor"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt-get install -y $REQUIRED_PKG
-fi
-
 echo "=========================== GNOME ==========================="
 sudo apt install -y gnome-shell-extensions
-
-REQUIRED_PKG="chrome-gnome-shell"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt -y install $REQUIRED_PKG
-fi
 
 echo "=========================== flameshot ==========================="
 sudo apt install -y flameshot
