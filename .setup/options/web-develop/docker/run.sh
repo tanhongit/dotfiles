9b18d1bea91989954c401e7c2691185f75ca9750
Tan Nguyen <tannp27@gmail.com>
2022-08-05 19:07:27 +0700

    update install docker to setup process

--- .setup/options/web-develop/docker/run.sh
+++ .setup/options/web-develop/docker/run.sh
@@ -3,6 +3,7 @@ COMMAND_NAME="docker"
 if ! command -v $COMMAND_NAME &>/dev/null; then
     echo "$COMMAND_NAME could not be found. Setting up $COMMAND_NAME."
     sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
     sudo apt update
     apt-cache policy docker-ce
