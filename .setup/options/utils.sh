e70ce35ffef837eff52c842e0ce77471132ca6f1
Tan Nguyen <tannp27@gmail.com>
2022-08-18 11:26:43 +0700

    add anydesk to utils setup

--- .setup/options/utils.sh
+++ .setup/options/utils.sh
@@ -33,3 +33,25 @@ for packageName in "${UTILS_PACKAGE_LIST[@]}"; do
     fi
     echo ""
 done
+
+echo "=========================== anydesk ==========================="
+COMMAND_NAME="anydesk"
+if ! command -v $COMMAND_NAME &>/dev/null; then
+    echo "$COMMAND_NAME could not be found. Setting up $COMMAND_NAME."
+    while true; do
+        read -p "Do you wish to install this program? (Y/N)  " yn
+        case $yn in
+        [Yy]*)
+            wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+            echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+            sudo apt update
+            sudo apt install $COMMAND_NAME -y
+            break
+            ;;
+        [Nn]*) break ;;
+        *) echo "Please answer yes or no." ;;
+        esac
+    done
+else
+    echo "$COMMAND_NAME install ok installed"
+fi
