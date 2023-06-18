d2b8da72ef26f5dc75bc84642c090dba88f64522
Tan Nguyen <tannp27@gmail.com>
2023-06-18 21:21:42 +0700

    fix shellcheck after add to github action

--- .backup/run.sh
+++ .backup/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.local/share/gnome-shell ../.local/share/gnome-shell
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.local/share/grilo-plugins ../.local/share/grilo-plugins
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.local/share/sounds ../.local/share/sounds
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.local/share/gnome-shell ../.local/share/gnome-shell
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.local/share/grilo-plugins ../.local/share/grilo-plugins
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.local/share/sounds ../.local/share/sounds
 
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.psensor ../.psensor
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.psensor ../.psensor
 
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/autostart ../.config/autostart
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/compton ../.config/compton
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/bleachbit ../.config/bleachbit
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/dconf ../.config/dconf
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/gnome-shell ../.config/gnome-shell
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/nautilus ../.config/nautilus
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/terminator ../.config/terminator
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/ulauncher ../.config/ulauncher
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.config/mimeapps.list ../.config/mimeapps.list
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/autostart ../.config/autostart
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/compton ../.config/compton
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/bleachbit ../.config/bleachbit
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/dconf ../.config/dconf
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/gnome-shell ../.config/gnome-shell
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/nautilus ../.config/nautilus
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/terminator ../.config/terminator
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/ulauncher ../.config/ulauncher
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.config/mimeapps.list ../.config/mimeapps.list
 
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.bashrc ../.bashrc
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.p10k.zsh ../.p10k.zsh
-cp -TRv ${ZSH_CUSTOM:-$HOME}/.zshrc ../.zshrc
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.bashrc ../.bashrc
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.p10k.zsh ../.p10k.zsh
+cp -TRv "${ZSH_CUSTOM:-$HOME}"/.zshrc ../.zshrc
