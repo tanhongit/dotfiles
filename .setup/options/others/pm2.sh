4a5018817a7dc1f1428f5e57ff6480bfc4b8a6d4
Tan Nguyen <tannp27@gmail.com>
2023-06-21 09:35:09 +0700

    feat: add pm2

---
+++ .setup/options/others/pm2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check if nvm, node installed and install pm2
+if ! command -v "nvm" &>/dev/null; then
+    echo "nvm could not be found. Setting up nvm."
+    curl https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+    source "$HOME/.profile" || source "$HOME/.bashrc"
+
+    nvm install node
+    nvm use node
+    nvm alias default node
+fi
+
+if ! command -v "pm2" &>/dev/null; then
+    echo "pm2 could not be found. Setting up pm2."
+    sudo npm install -g pm2
+fi
