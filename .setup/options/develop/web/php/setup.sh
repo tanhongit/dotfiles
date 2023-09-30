e0584425f5d49d524b11f25f136382dde073e25c
Tan Nguyen <tannp27@gmail.com>
2023-09-30 16:06:47 +0700

    config: install phpmyadmin for only apache

--- .setup/options/develop/web/php/setup.sh
+++ .setup/options/develop/web/php/setup.sh
@@ -13,18 +13,6 @@ echo ''
 echo "=========================== Web Server ==========================="
 bash web-server.sh
 
-echo "=========================== phpmyadmin ==========================="
-REQUIRED_PKG="phpmyadmin"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo "Checking for $REQUIRED_PKG: $PKG_OK"
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt install -y $REQUIRED_PKG php-json
-    sudo phpenmod mbstring
-    sudo systemctl restart apache2
-fi
-echo ''
-
 installAptDevPackages() {
     PACKAGE_LIST=("filezilla" "software-properties-common" "apt-transport-https" "gpg")
 
