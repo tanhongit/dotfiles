2ab788137b7a762412a716b897f8eac75683ae16
Tan Nguyen <tannp27@gmail.com>
2023-10-01 13:13:51 +0700

    fix syntax error

--- .setup/options/packages/chrome.sh
+++ .setup/options/packages/chrome.sh
@@ -2,7 +2,7 @@
 
 COMMAND_NAME="google-chrome"
 if ! [ -x "$(command -v $COMMAND_NAME)" ]; then
-    cho "$COMMAND_NAME could not be found. Setting up $COMMAND_NAME."
+    echo "$COMMAND_NAME could not be found. Setting up $COMMAND_NAME."
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
 else
