9a7878327b236075035d6e1fe4a9f83f4b71041a
Tan Nguyen <tannp27@gmail.com>
2024-02-08 23:32:38 +0700

    add some extensions for php installer

--- .setup/options/develop/web/php/php.sh
+++ .setup/options/develop/web/php/php.sh
@@ -87,7 +87,7 @@ if ! command -v $COMMAND_NAME &>/dev/null; then
     done
 
     phpExtensions() {
-        sudo apt install php"$PHP_VERSION" php"$PHP_VERSION"-common php"$PHP_VERSION"-mysql php"$PHP_VERSION"-curl php"$PHP_VERSION"-gd php"$PHP_VERSION"-redis php"$PHP_VERSION"-mbstring php"$PHP_VERSION"-xml php"$PHP_VERSION"-zip php"$PHP_VERSION"-readline -y
+        sudo apt install php"$PHP_VERSION" php"$PHP_VERSION"-common php"$PHP_VERSION"-mysql php"$PHP_VERSION"-curl php"$PHP_VERSION"-gd php"$PHP_VERSION"-redis php"$PHP_VERSION"-mbstring php"$PHP_VERSION"-xml php"$PHP_VERSION"-zip php"$PHP_VERSION"-readline php"$PHP_VERSION"-bcmath php"$PHP_VERSION"-intl php"$PHP_VERSION"-json php"$PHP_VERSION"-opcache php"$PHP_VERSION"-soap php"$PHP_VERSION"-sqlite3 php"$PHP_VERSION"-xmlrpc php"$PHP_VERSION"-xsl php"$PHP_VERSION"-imagick php"$PHP_VERSION"-dev php"$PHP_VERSION"-cli php"$PHP_VERSION"-curl
     }
 
     if [[ $WEB_SERVER == "apache2" ]]; then
