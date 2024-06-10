973eb723c95379cd2019e4e6fb3c4dce5f2d0aa7
Tan Nguyen <tannp27@gmail.com>
2024-06-11 00:26:56 +0700

    feat: add some apps for macOS

--- macos/install.sh
+++ macos/install.sh
@@ -30,7 +30,7 @@ done
 
 echo '####################################################################'
 brewInstallation() {
-    APP_LIST=("skype" "spotify" "anydesk" "obs" "chatgpt" "slack" "gpg-suite" "notion" "zoom" "figma")
+    APP_LIST=("skype" "spotify" "anydesk" "teamviewer" "obs" "chatgpt" "slack" "gpg-suite" "notion" "zoom" "figma")
 
     for appName in "${APP_LIST[@]}"; do
         echo "=========================== $appName ==========================="
@@ -62,6 +62,41 @@ brewInstallation() {
     done
 }
 brewInstallation
+
+
+brewFormulaInstallation() {
+    APP_LIST=("flameshot")
+
+    for appName in "${APP_LIST[@]}"; do
+        echo "=========================== $appName ==========================="
+
+        PKG_OK=$(brew list --formula | grep "^$appName$")
+        echo "Checking for $appName: $PKG_OK"
+        if [ "" = "$PKG_OK" ]; then
+            echo "No $appName. Setting up $appName."
+
+            while true; do
+                if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
+                    yn="y"
+                else
+                    read -r -p "Do you want to install $appName? (Y/N)  " yn
+                fi
+                case $yn in
+                [Yy]*)
+                    brew install "$appName"
+                    break
+                    ;;
+                [Nn]*) break ;;
+                *) echo "Please answer yes or no." ;;
+                esac
+            done
+        else
+            echo "$appName install ok installed"
+        fi
+        echo ""
+    done
+}
+brewFormulaInstallation
 
 if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
     sudo spctl --master-enable
