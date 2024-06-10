7f102d5c585ceb8f34e2178ea47e368d7bf93b0a
Tan Nguyen <tannp27@gmail.com>
2024-06-10 11:49:06 +0700

    fix: add select choose for install app in macOS

--- macos/run.sh
+++ macos/run.sh
@@ -13,7 +13,21 @@ for appName in "${APP_LIST[@]}"; do
     echo "Checking for $REQUIRED_PKG: $PKG_OK"
     if [ "" = "$PKG_OK" ]; then
         echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-        brew install --cask "$REQUIRED_PKG"
+
+        if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
+            yn="y"
+        else
+            read -r -p "Do you want to install $REQUIRED_PKG? (Y/N)  " yn
+        fi
+
+        case $yn in
+        [Yy]*)
+            brew install --cask "$REQUIRED_PKG"
+            break
+            ;;
+        [Nn]*) break ;;
+        *) echo "Please answer yes or no." ;;
+        esac
     fi
     echo ""
 done
