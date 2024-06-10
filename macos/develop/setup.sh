1caf4e9d4fbaecd1715cdd7e0892679d9295624b
Tan Nguyen <tannp27@gmail.com>
2024-06-10 22:42:09 +0700

    feat: add orbstack (formula) for macOS

--- macos/develop/setup.sh
+++ macos/develop/setup.sh
@@ -34,6 +34,39 @@ brewInstallation() {
 }
 brewInstallation
 
+brewFormulaInstallation() {
+    APP_LIST=("orbstack")
+
+    for appName in "${APP_LIST[@]}"; do
+        echo "=========================== $appName ==========================="
+
+        PKG_OK=$(brew list --formula | grep "^$appName$")
+        echo "Checking for $appName: $PKG_OK"
+        if [ "" = "$PKG_OK" ]; then
+            echo "No $appName. Setting up $appName."
+
+            while true; do
+                if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
+                    yn="y"
+                else
+                    read -r -p "Do you want to install $appName? (Y/N)  " yn
+                fi
+                case $yn in
+                [Yy]*)
+                    brew install "$appName"
+                    break
+                    ;;
+                [Nn]*) break ;;
+                *) echo "Please answer yes or no." ;;
+                esac
+            done
+        else
+            echo "$appName install ok installed"
+        fi
+        echo ""
+    done
+}
+
 echo '####################################################################'
 while true; do
     if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
