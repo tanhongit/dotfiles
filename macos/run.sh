a53cd087a3e2d21896b79c286cb0017d578bd483
Tan Nguyen <tannp27@gmail.com>
2024-06-10 10:35:09 +0700

    fix: ignore checksum verifications

--- macos/run.sh
+++ macos/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
+    brew tap homebrew/cask
+fi
+
 brew install --cask skype spotify anydesk phpstorm webstorm postman
+
+if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
+    sudo spctl --master-enable  # Re-enable Gatekeeper
+fi
