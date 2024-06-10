93e884ad89e9dac474e5ebe47944f8fba942ce58
Tan Nguyen <tannp27@gmail.com>
2024-06-10 10:53:21 +0700

    refactor: to install cask list

--- macos/run.sh
+++ macos/run.sh
@@ -4,8 +4,13 @@ if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
     brew tap homebrew/cask
 fi
 
-brew install --cask skype spotify anydesk phpstorm webstorm postman
+apps=(skype spotify anydesk phpstorm webstorm postman)
+for app in "${apps[@]}"; do
+    output=$(brew install --cask $app 2>&1)
+    clean_output=$(echo "$output" | grep -v "Warning: macOS's Gatekeeper has been disabled for this Cask")
+    echo "$clean_output"
+done
 
 if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
-    sudo spctl --master-enable  # Re-enable Gatekeeper
+    sudo spctl --master-enable
 fi
