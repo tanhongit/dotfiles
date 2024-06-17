2f4eabf1563cf7462b536f2a7c439bf4138a4fed
Tan Nguyen <tannp27@gmail.com>
2024-06-18 00:41:45 +0700

    fix: remove redundant variable in bash files

--- linux/options/develop/web/list.sh
+++ linux/options/develop/web/list.sh
@@ -5,12 +5,11 @@ installSnapDevPackages() {
 
     for packageName in "${PACKAGE_LIST[@]}"; do
         echo "=========================== $packageName ==========================="
-        COMMAND_NAME=$packageName
-        if ! command -v "$COMMAND_NAME" &>/dev/null; then
-            echo "$COMMAND_NAME could not be found. Setting up $COMMAND_NAME."
-            sudo snap install "$COMMAND_NAME"
+        if ! command -v "$packageName" &>/dev/null; then
+            echo "$COMMAND_NAME could not be found. Setting up $packageName."
+            sudo snap install "$packageName"
         else
-            echo "$COMMAND_NAME install ok installed"
+            echo "$packageName install ok installed"
         fi
         echo ""
     done
