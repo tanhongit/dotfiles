d5e02534ee39642f9ff4346ec66432e0bf1d694c
Tan Nguyen <tannp27@gmail.com>
2024-06-20 15:22:23 +0700

    :+1: feat: add config for Dock in macOS

--- macos/Preferences/config.sh
+++ macos/Preferences/config.sh
@@ -34,3 +34,29 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # show warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool true
+
+killall Finder
+
+# ===================== Dock ===================== #
+defaults write com.apple.dock autohide -bool false
+defaults write com.apple.dock expose-group-apps -bool false
+defaults write com.apple.dock largesize -float 60
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock mineffect -string "genie"
+defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock orientation -string "bottom"
+defaults write com.apple.dock show-process-indicators -bool true
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock showAppExposeGestureEnabled -bool true
+defaults write com.apple.dock tilesize -float 45
+defaults write com.apple.dock trash-full -bool true
+defaults write com.apple.dock wvous-bl-corner -int 2
+defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 14
+defaults write com.apple.dock wvous-tl-corner -int 1
+defaults write com.apple.dock wvous-tl-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 1
+defaults write com.apple.dock wvous-tr-modifier -int 1048576
+defaults write com.apple.dock workspaces-swoosh-animation-off -bool false
+killall Dock
