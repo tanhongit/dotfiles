1e0fa753e39555da60ab4c84177c8cf6bf1bd48b
Tan Nguyen <tannp27@gmail.com>
2024-06-20 20:02:26 +0700

    :+1: feat: add config for Rectangle and keepyouawake in macOS

--- macos/Preferences/config.sh
+++ macos/Preferences/config.sh
@@ -64,11 +64,41 @@ killall Dock
 # ===================== Maccy ===================== #
 # Maccy is a clipboard manager for macOS
 # defaults read org.p0deje.Maccy
-defaults write org.p0deje.Maccy historySize 999
-defaults write org.p0deje.Maccy menuIcon clipboard
-defaults write org.p0deje.Maccy sortBy lastCopiedAt
-defaults write org.p0deje.Maccy showSpecialSymbols 1
-defaults write org.p0deje.Maccy popupPosition cursor
-defaults write org.p0deje.Maccy hideFooter 0
-defaults write org.p0deje.Maccy hideSearch 0
-defaults write org.p0deje.Maccy hideTitle 0
+if [[ -d "/Applications/Maccy.app" ]]; then
+    defaults write org.p0deje.Maccy historySize 999
+    defaults write org.p0deje.Maccy menuIcon clipboard
+    defaults write org.p0deje.Maccy sortBy lastCopiedAt
+    defaults write org.p0deje.Maccy showSpecialSymbols 1
+    defaults write org.p0deje.Maccy popupPosition cursor
+    defaults write org.p0deje.Maccy hideFooter 0
+    defaults write org.p0deje.Maccy hideSearch 0
+    defaults write org.p0deje.Maccy hideTitle 0
+    echo "Maccy is configured"
+fi
+
+# ===================== Rectangle ===================== #
+# Rectangle is a window management app for macOS
+# defaults read com.knollsoft.Rectangle
+if [[ -d "/Applications/KeepingYouAwake.app" ]]; then
+    defaults write com.knollsoft.Rectangle SUEnableAutomaticChecks 1
+    defaults write com.knollsoft.Rectangle SUHasLaunchedBefore 1
+    defaults write com.knollsoft.Rectangle SUHasLaunchedSinceUpdate 1
+    defaults write com.knollsoft.Rectangle launchOnLogin 1
+    defaults write com.knollsoft.Rectangle hideMenubarIcon 0
+    defaults write com.knollsoft.Rectangle moveCursorAcrossDisplays 1
+    defaults write com.knollsoft.Rectangle doubleClickTitleBar 3
+    defaults write com.knollsoft.Rectangle allowAnyShortcut 1
+    defaults write com.knollsoft.Rectangle alternateDefaultShortcuts 1
+fi
+
+# ===================== keepyouawake ===================== #
+# keepyouawake is a menu bar app to prevent your Mac from going to sleep
+# defaults read info.marcel-dierkes.KeepingYouAwake
+if [[ -d "/Applications/KeepingYouAwake.app" ]]; then
+    defaults write info.marcel-dierkes.KeepingYouAwake SUEnableAutomaticChecks 1
+    defaults write info.marcel-dierkes.KeepingYouAwake SUHasLaunchedBefore 1
+    defaults write info.marcel-dierkes.KeepingYouAwake "info.marcel-dierkes.KeepingYouAwake.BatteryCapacityThreshold" -int 25
+    defaults write info.marcel-dierkes.KeepingYouAwake "info.marcel-dierkes.KeepingYouAwake.BatteryCapacityThresholdEnabled" -int 1
+    defaults write info.marcel-dierkes.KeepingYouAwake "info.marcel-dierkes.KeepingYouAwake.LowPowerModeMonitoringEnabled" -int 1
+    killall KeepingYouAwake
+fi
