5a33be4688048ffc3c3b87c551911ed1fd70dc82
Tan Nguyen <tannp27@gmail.com>
2024-06-20 08:24:00 +0700

    :+1: update latest npm version

--- macos/develop/js/setup.sh
+++ macos/develop/js/setup.sh
@@ -8,25 +8,25 @@ for appName in "${APP_LIST[@]}"; do
     PKG_OK=$(brew list --cask | grep "^$appName$")
     echo "Checking for $appName: $PKG_OK"
     if [ "" = "$PKG_OK" ]; then
-            echo "No $appName. Setting up $appName."
+        echo "No $appName. Setting up $appName."
 
-            while true; do
-                if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
-                    yn="y"
-                else
-                    read -r -p "Do you want to install $appName? (Y/N)  " yn
-                fi
-                case $yn in
-                [Yy]*)
-                    brew install --cask "$appName"
-                    break
-                    ;;
-                [Nn]*) break ;;
-                *) echo "Please answer yes or no." ;;
-                esac
-            done
-        else
-            echo "$appName install ok installed"
-        fi
+        while true; do
+            if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
+                yn="y"
+            else
+                read -r -p "Do you want to install $appName? (Y/N)  " yn
+            fi
+            case $yn in
+            [Yy]*)
+                brew install --cask "$appName"
+                break
+                ;;
+            [Nn]*) break ;;
+            *) echo "Please answer yes or no." ;;
+            esac
+        done
+    else
+        echo "$appName install ok installed"
+    fi
     echo ""
 done
--- macos/develop/packages/nvm.sh
+++ macos/develop/packages/nvm.sh
@@ -2,6 +2,6 @@
 
 # Install nvm
 if [ ! -s "$HOME/.nvm/nvm.sh" ]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
     nvm install --lts
 fi
