450d52664aa5a9ed49cf9848b0530badc2ca84cb
Tan Nguyen <tannp27@gmail.com>
2024-06-20 11:02:45 +0700

    :+1: fix: add conditions for git config global both os

--- global/git-config.sh
+++ global/git-config.sh
@@ -6,13 +6,31 @@ mkdir -p "$HOME"/.config/git
 
 cp -Rfv ../.config/git/ignore "$HOME"/.config/git/ignore
 
-# set git config core to ignore file
+# ================ set git config core to ignore file ================
 if [ -z "$(git config --global core.excludesfile)" ]; then
     git config --global core.excludesfile "$HOME"/.config/git/ignore
-    git config --global core.ignorecase false
-    git config --global core.autocrlf input
-    git config --global core.safecrlf true
     echo "Git config core.excludesfile is set"
 else
   echo "Git config core.excludesfile is ready"
+fi
+
+if [ -z "$(git config --global core.ignorecase)" ]; then
+    git config --global core.ignorecase false
+    echo "Git config core.ignorecase is set"
+else
+  echo "Git config core.ignorecase is ready"
+fi
+
+if [ -z "$(git config --global core.autocrlf)" ]; then
+    git config --global core.autocrlf input
+    echo "Git config core.autocrlf is set"
+else
+  echo "Git config core.autocrlf is ready"
+fi
+
+if [ -z "$(git config --global core.safecrlf)" ]; then
+    git config --global core.safecrlf true
+    echo "Git config core.safecrlf is set"
+else
+  echo "Git config core.safecrlf is ready"
 fi
