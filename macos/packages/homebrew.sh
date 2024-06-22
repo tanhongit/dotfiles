1be276f33460b74fa344e28b9b183e7a164c67fc
Tan Nguyen <tannp27@gmail.com>
2024-06-22 10:07:52 +0700

    Update homebrew.sh

--- macos/packages/homebrew.sh
+++ macos/packages/homebrew.sh
@@ -6,4 +6,6 @@ xcode-select --install
 
 if test ! "$(which brew)"; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
